32beefddf1578c5e51e283ed85771844d269e9bb
AsifMahmudSiam <62529690+AsifMahmudSiam@users.noreply.github.com>
2020-04-01 00:32:57 +0600

    User InterFace Done

---
+++ Blood_Bank_Management_System/GUI/user.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Blood_Bank_Management_System.GUI
+{
+    public partial class formuser : Form
+    {
+        public formuser()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSelectPicture_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void formuser_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+    }
+}
